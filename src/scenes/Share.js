23a4348d42f80626ccb93b104588703f04fbcdcf
Paulo Campos <paulo.campos@studiocuboweb.com.br>
2020-09-10 22:43:23 -0300

    changed url

--- src/scenes/Share.js
+++ src/scenes/Share.js
@@ -75,7 +75,7 @@ const Buttons = styled.div`
 const Share = ({ intl,lastPath }) => {
   const title = intl.formatMessage(messages.title);
   const siteTitle = intl.formatMessage(messages.siteTitle);
-  const url = process.env.SITE_URL || "http://google.com";
+  const url = process.env.SITE_URL || "https://omatacavalo.infoamazonia.org";
   const {
     FacebookShareButton,
     GooglePlusShareButton,
